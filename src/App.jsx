dd394113967f706b5fa86648b456586f34a978a3
devhaider <rthafizulhaider@gmail.com>
2026-01-17 10:35:44 +0600

    Card Design Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -80,7 +81,7 @@ function App() {
             <ul>
               {
                 movieList.map ((movie) => (
-                  <p key={movie.id} className="text-white">{movie.title}</p>
+                  <MovieCard key={movie.id} movie={movie} />
                 ))}
             </ul>
           )
